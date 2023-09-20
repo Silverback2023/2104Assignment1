fd36ea2505cf636ae23c0a47c7bf90688d555aa7
Silverback2023 <sanctified_rn@att.net>
2023-09-20 14:14:16 -0700

    Initial commit

---
+++ Assignment1/BMIPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+
+
+namespace Assignment1
+{
+    public sealed partial class BMIPage : Page
+    {
+        public BMIPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+
+        private void btnHome_Click_1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void btnCalculateBMI_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(heightCM.Text, out double height) && double.TryParse(weightKG.Text, out double weight))
+            {
+                // Calculate BMI
+                double bmi = weight / ((height / 100) * (height / 100));
+
+                // Display the result in bmiResult TextBox
+                bmiResult.Text = $"Your BMI is: {bmi:F2}";
+            }
+            else
+            {
+                // Handle invalid input
+                bmiResult.Text = "Please enter valid height and weight.";
+            }
+        }
+    }
+}
---
+++ Assignment1/TipCalculatorPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+
+namespace Assignment1
+{
+    public sealed partial class TipCalculatorPage : Page
+    {
+        public TipCalculatorPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+        private void btnCalculateTip_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(billAmountTextBox.Text, out double bill) && double.TryParse(tipPercentTextBox.Text, out double tipPercentage))
+            {
+                double tip = (bill * tipPercentage) / 100;
+                double totalBill = bill + tip;
+
+                // Display the results in the tipResultTextBlock
+                tipResultTextBlock.Text = $"Tip: {tip:C}\nTotal Bill: {totalBill:C}";
+            }
+            else
+            {
+                // Handle invalid input
+                tipResultTextBlock.Text = "Invalid input. Please enter valid bill amount and tip percentage.";
+            }
+        }
+
+        private void btnHome_Click_1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+    }
+}
+
